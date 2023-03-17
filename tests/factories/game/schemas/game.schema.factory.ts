da619e9222500e6c1d8845598a5b64a0d97cbfdf
Antoine Zanardi <antoine.zanardi@epitech.eu>
2023-03-17 21:42:28 +0100

    chore(project): tree structure cleaned (#30)

--- tests/factories/game/schemas/game.schema.factory.ts
+++ tests/factories/game/schemas/game.schema.factory.ts
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
-import { GAME_PHASES, GAME_STATUSES } from "../../../../src/game/enums/game.enum";
-import type { Game } from "../../../../src/game/schemas/game.schema";
+import { GAME_PHASES, GAME_STATUSES } from "../../../../src/modules/game/enums/game.enum";
+import type { Game } from "../../../../src/modules/game/schemas/game.schema";
 import { createFakeGameOptions } from "./game-options/game-options.schema.factory";
 import { bulkCreateFakePlayers } from "./player/player.schema.factory";
 
---
+++ tests/unit/specs/shared/api/helpers/api.helper.spec.ts
@@ -0,0 +1,10 @@
+import { API_RESOURCES } from "../../../../../../src/shared/api/enums/api.enum";
+import { getResourceSingularForm } from "../../../../../../src/shared/api/helpers/api.helper";
+
+describe("API Helper", () => {
+  describe("getResourceSingularForm", () => {
+    it.each<{ resource: API_RESOURCES; singular: string }>([{ resource: API_RESOURCES.GAMES, singular: "game" }])("should return $singular when called with $resource [#$#].", ({ resource, singular }) => {
+      expect(getResourceSingularForm(resource)).toBe(singular);
+    });
+  });
+});
