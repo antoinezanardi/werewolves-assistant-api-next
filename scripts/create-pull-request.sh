1c3d1fb4e6c757a8977ed88b7ebc6a23e5e69163
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2023-03-28 14:02:29 +0200

    chore(scripts): set assignee in create pull request script

--- scripts/create-pull-request.sh
+++ scripts/create-pull-request.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+github_user=$(git config github.user)
+
+while true; do
+    if [[ -z "$github_user" ]]; then
+        read -r -p "Enter your GitHub username: " github_user
+        git config github.user "$github_user"
+    elif ! curl -s "https://api.github.com/users/$github_user" | grep -q '"login":'; then
+        echo "The GitHub username \"$github_user\" does not exist or is invalid."
+        read -r -p "Enter your GitHub username: " github_user
+        git config github.user "$github_user"
+    else
+        break
+    fi
+done
+
 base_branch=${1:-develop}
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
@@ -33,15 +48,35 @@ pr_url=$(curl -s "https://api.github.com/repos/$username/$repository/pulls?head=
 if [ -n "$pr_url" ]; then
   echo "❌  A pull request already exists for $current_branch against $base_branch: $pr_url"
   exit 1
-else
-  git push -u origin "$current_branch"
+fi
 
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    open "https://github.com/$username/$repository/compare/$base_branch...$current_branch?expand=1"
-  elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    xdg-open "https://github.com/$username/$repository/compare/$base_branch...$current_branch?expand=1"
-  else
-    echo "❌  Unsupported OS for automatically open the pull request creation page"
-    exit 1
+git push -u origin "$current_branch"
+
+word_map=("feat:🚀 feature" "fix:🐛 bug" "chore:🧹 chore" "docs:📖 docs" "style:🎨 style" "refactor:🔩 refactor" "perf:⚡️ perf" "test:✅ test" "ci:🔁 ci")
+
+first_commit_message=$(git log --format=%B -n 1 "$base_branch".."$current_branch")
+
+first_word=$(echo "$first_commit_message" | cut -d'(' -f1)
+
+label=""
+for pair in "${word_map[@]}"; do
+  key=${pair%%:*}
+  value=${pair#*:}
+  if [ "$key" = "$first_word" ]; then
+    label=$value
+    break
   fi
+done
+
+open_pr_url="https://github.com/$username/$repository/compare/$base_branch...$current_branch?expand=1&assignee=$github_user"
+encoded_label=$(ruby -e 'require "erb"; puts ERB::Util.url_encode(ARGV[0])' "$label")
+encoded_title=$(ruby -e 'require "erb"; puts ERB::Util.url_encode(ARGV[0])' "$first_commit_message")
+encoded_open_pr_url="$open_pr_url&labels=$encoded_label&title=$encoded_title"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  open "$encoded_open_pr_url"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  xdg-open "$encoded_open_pr_url"
+else
+  echo "❌  Unsupported OS for automatically open the pull request creation page"
+  exit 1
 fi
