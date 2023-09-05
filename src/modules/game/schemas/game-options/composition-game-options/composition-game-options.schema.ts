b6d937f37fde34ad7f58128788abf37dfc32b2ba
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2023-09-05 11:47:54 +0200

    refactor(constants): constants for game schemas

--- src/modules/game/schemas/game-options/composition-game-options/composition-game-options.schema.ts
+++ src/modules/game/schemas/game-options/composition-game-options/composition-game-options.schema.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
 
-import { COMPOSITION_GAME_OPTIONS_API_PROPERTIES, COMPOSITION_GAME_OPTIONS_FIELDS_SPECS } from "@/modules/game/constants/game-options/composition-game-options.constant";
+import { COMPOSITION_GAME_OPTIONS_API_PROPERTIES, COMPOSITION_GAME_OPTIONS_FIELDS_SPECS } from "@/modules/game/schemas/game-options/composition-game-options/composition-game-options.schema.constant";
 
 @Schema({
   versionKey: false,
