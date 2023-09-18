a02a4ec90da3092ba1c882dd5da5fde9c646f0b6
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2023-09-18 21:44:09 +0200

    feat(roles): role's origin

--- tests/unit/specs/modules/role/helpers/role.factory.spec.ts
+++ tests/unit/specs/modules/role/helpers/role.factory.spec.ts
@@ -1,4 +1,4 @@
-import { RoleNames, RoleSides, RoleTypes } from "@/modules/role/enums/role.enum";
+import { RoleNames, RoleOrigins, RoleSides, RoleTypes } from "@/modules/role/enums/role.enum";
 import { createRole } from "@/modules/role/helpers/role.factory";
 import type { Role } from "@/modules/role/types/role.type";
 
@@ -11,6 +11,7 @@ describe("Role Factory", () => {
         name: RoleNames.ANCIENT,
         type: RoleTypes.AMBIGUOUS,
         side: RoleSides.VILLAGERS,
+        origin: RoleOrigins.CHARACTERS,
         maxInGame: 1,
         minInGame: 1,
       };
