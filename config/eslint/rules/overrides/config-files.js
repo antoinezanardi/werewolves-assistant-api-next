17fa843a8e603dd48ba3a51792504fa89798e55a
Antoine ZANARDI <antoine.zanardi@epitech.eu>
2023-10-16 14:44:11 +0200

    ci(stryker): dedicated config for stryker in ci

--- config/eslint/rules/overrides/config-files.js
+++ config/eslint/rules/overrides/config-files.js
@@ -1,9 +1,11 @@
 const { OFF } = require("../../constants");
 
 const configFilesOverride = Object.freeze({
-  files: ["./config/**/*.ts"],
+  files: ["./config/**/*.ts", "./config/**/*.js"],
   rules: {
+    "@typescript-eslint/no-require-imports": OFF,
     "@typescript-eslint/no-restricted-imports": OFF,
+    "@typescript-eslint/no-var-requires": OFF,
     "import/no-default-export": OFF,
     "import/no-relative-parent-imports": OFF,
   },
